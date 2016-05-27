97a57e109281fa57ec3d413c9b8231daa604a27f
Jay Taylor <outtatime@gmail.com>
2016-05-27 16:51:28 -0700

    Fixed netlib.IsTcpPortReachable test broken by function rename.

--- is_tcp_port_reachable_test.go
+++ is_tcp_port_reachable_test.go
@@ -15,13 +15,13 @@ func TestIsTcpPortReachable(t *testing.T) {
 		t.Fatalf("Error starting WebServer: %s", err)
 	}
 	addr := server.Addr().String()
-	if expected, actual := true, netlib.IsPortReachable(addr); actual != expected {
+	if expected, actual := true, netlib.IsTcpPortReachable(addr); actual != expected {
 		t.Errorf("Expected address %v to be reachable but actual result=%v", addr, actual)
 	}
 	if err := server.Stop(); err != nil {
 		t.Fatalf("Error stopping WebServer: %s", err)
 	}
-	if expected, actual := false, netlib.IsPortReachable(addr); actual != expected {
+	if expected, actual := false, netlib.IsTcpPortReachable(addr); actual != expected {
 		t.Errorf("Expected address %v to be unreachable after server is shutdown but actual=%v", addr, actual)
 	}
 }
